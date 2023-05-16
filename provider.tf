64681443dab1cd0ebc99a8a89849c8f574a219dd
Moutaz Muhammad <moutazmuhamad@gmail.com>
2023-05-16 22:25:17 +0300

    update

--- provider.tf
+++ provider.tf
@@ -1,4 +1,13 @@
 # Configure the AWS Provider
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.0"
+    }
+  }
+}
+
 provider "aws" {
   region = var.region
 }
