c437cb80eb0275bfa505488e230bea769ae033c1
Moutaz Muhammad <moutazmuhamad@gmail.com>
2023-08-20 18:20:41 +0300

    update

--- backend.tf
+++ backend.tf
@@ -14,16 +14,3 @@ terraform {
     dynamodb_table = "terraform-state-lock-dynamo"
   }
 }
-
-resource "aws_dynamodb_table" "dynamodb-terraform-state-lock" {
-  name = "terraform-state-lock-dynamo"
-  hash_key = "LockID"
-  read_capacity = 20
-  write_capacity = 20
- 
-  attribute {
-    name = "LockID"
-    type = "S"
-  }
-}
-
