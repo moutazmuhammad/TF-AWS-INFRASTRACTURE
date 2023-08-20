ca40b80d8087f1e047602448d7eaf95beecbfc7f
Moutaz Muhammad <moutazmuhamad@gmail.com>
2023-08-20 23:25:09 +0300

    update

--- Modules/VM/sg.tf
+++ Modules/VM/sg.tf
@@ -4,14 +4,6 @@ resource "aws_security_group" "public_sg" {
   vpc_id      = var.vpc_id
 
   ingress {
-    description = "http from VPC"
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  ingress {
     description = "Alllow ssh"
     from_port   = 22
     to_port     = 22
@@ -37,26 +29,11 @@ resource "aws_security_group" "private_sg" {
   vpc_id = var.vpc_id
 
   ingress {
-    description = "http from VPC"
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  ingress {
     description = "ssh from VPC"
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
+    security_groups = [aws_security_group.public_sg.id] 
   }
 
   tags = {
