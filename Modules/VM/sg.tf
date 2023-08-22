260058858cbd4a79f2c07054c589aa9289d9512b
Moutaz Muhammad <85679864+moutazmuhammad@users.noreply.github.com>
2023-08-22 16:43:43 +0300

    Update sg.tf

--- Modules/VM/sg.tf
+++ Modules/VM/sg.tf
@@ -36,6 +36,13 @@ resource "aws_security_group" "private_sg" {
     security_groups = [aws_security_group.public_sg.id] 
   }
 
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
   tags = {
     Name = "private-sg"
   }
